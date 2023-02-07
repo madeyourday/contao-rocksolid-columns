fe5a425ccf3b60e70ac24e31adea32bf97c08d08
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2023-02-07 17:56:21 +0100

    Add translations for cols + background config

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -13,9 +13,45 @@
  */
 
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_legend'] = 'Column configuration';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_large'][0] = 'Desktop XL columns';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_large'][1] = 'Example: 2, 3, 1-2-1, 1-3. Max. 12 cols. Explanation: The desired number of evenly distributed columns (e.g. 3) or a statement following the syntax 1-2-1 for 3 columns with a doubled middle column. Note: Per column you\'ll need one element. Single elements aren\'t split automatically.';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_large'][0] = 'Desktop columns';
-$GLOBALS['TL_LANG']['tl_content']['rs_columns_large'][1] = 'Example: 2, 3, 1-2-1, 1-3. Max. 6 cols. Explanation: The desired number of evenly distributed columns (e.g. 3) or a statement following the syntax 1-2-1 for 3 columns with a doubled middle column. Note: Per column you\'ll need one element. Single elements aren\'t split automatically.';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_large'][1] = 'Example: 2, 3, 1-2-1, 1-3. Max. 12 cols. (optional)';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_medium'][0] = 'Tablet columns';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_medium'][1] = 'Example: 2, instead of 1-1-2 on desktop... number and distribution of the columns for visitors with smaller resolution (optional).';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_small'][0] = 'Mobile columns';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_small'][1] = 'Usually 1 or 2... the distribution for even smaller screens (optional, but recommended).';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_xsmall'][0] = 'Mobile XS columns';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_xsmall'][1] = 'Usually 1 or 2... the distribution for even smaller screens (optional, but recommended).';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_gutter'][0] = 'Columns gutter';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_gutter'][1] = 'Size of the gutter between the columns.';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_gutters']['none'] = 'None';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_gutters']['s'] = 'Small';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_gutters']['m'] = 'Medium';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_gutters']['l'] = 'Large';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_outside_gutters'][0] = 'Outside gutter';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_outside_gutters'][1] = 'Inserts a space before the first and after the last column.';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_equal_height'][0] = 'Equal column height';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_equal_height'][1] = 'Creates the same height for all columns in a row.';
+
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_legend'] = 'Column background';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_color_inverted'][0] = 'Text color inverted';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_color_inverted'][1] = 'Light text color for dark backgrounds and photos.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background'][0] = 'Enable column background';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background'][1] = 'Creates a colored background or background graphic.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_color'][0] = 'Color';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_color'][1] = 'Color of the background. Leave this field empty for a transparent background.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_image'][0] = 'Image';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_image'][1] = 'Background image.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_image_size'][0] = 'Image width and height';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_image_size'][1] = 'Here you can set the dimensions of the image and the scaling mode.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_size'][0] = 'Background size';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_size'][1] = 'Image display size.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_sizes']['cover'] = 'Cropped';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_sizes']['contain'] = 'Proportional';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_sizes']['100% 100%'] = 'Distorted';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_sizes']['auto auto'] = 'Original size';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_position'][0] = 'Background position';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_position'][1] = 'Position of the background.';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_repeat'][0] = 'Background repeat';
+$GLOBALS['TL_LANG']['tl_content']['rs_column_background_repeat'][1] = 'Repeat mode of the background image.';
