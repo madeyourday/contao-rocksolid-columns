0bc925f54279942e68a392027598aec57b2d9cae
Martin Auswöger <martin@auswoeger.com>
2025-10-15 11:32:57 +0200

    Sticky alignment for columns

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -39,6 +39,7 @@ $GLOBALS['TL_LANG']['tl_content']['rs_columns_aligns']['center'] = 'Mitte';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_aligns']['bottom'] = 'Unten';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_aligns']['stretch'] = 'Höhe angleichen';
 $GLOBALS['TL_LANG']['tl_content']['rs_columns_aligns']['baseline'] = 'An Grundlinie ausrichten';
+$GLOBALS['TL_LANG']['tl_content']['rs_columns_aligns']['sticky'] = 'Sticky';
 
 $GLOBALS['TL_LANG']['tl_content']['rs_column_background_legend'] = 'Hintergrund der Spalte';
 $GLOBALS['TL_LANG']['tl_content']['rs_column_color_inverted'][0] = 'Textfarbe invertieren';
